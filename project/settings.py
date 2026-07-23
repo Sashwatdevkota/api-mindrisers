841c032eb82ee1da4a0832753bccb46f89d51e91
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-07-23 22:08:40 +0545

    added a base template with basic index page

--- project/settings.py
+++ project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "Templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
