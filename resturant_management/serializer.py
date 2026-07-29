9ea44b377f671efa93c235a3f95b1ee0b4c0e186
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-07-29 08:30:38 +0545

    API POST method

--- resturant_management/serializer.py
+++ resturant_management/serializer.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+from .models import *
 
 
 class CategorySerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
+
+    def create(self, validated_data):
+        return Category.objects.create(name=validated_data.get("name"))
 
 
 class TableSerializer(serializers.Serializer):
--- resturant_management/views.py
+++ resturant_management/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
 from rest_framework.response import Response
-from .models import *
-from .serializer import *
+from .models import *  # importing all models
+from .serializer import *  # importing all serializers(converting queryset to json)
 
 # Create your views here.
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def category_list(request):
-    category = Category.objects.all()
-    serializer = CategorySerializer(category, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": "Data added", "result": serializer.data})
 
 
 @api_view(["GET"])
