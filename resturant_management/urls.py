7df1b46ed96b852830b206e0a3aa7280bf5f5642
Sashwatdevkota <sashwatdevkota17@gmail.com>
2026-07-31 07:39:36 +0545

    generic APIs

--- resturant_management/urls.py
+++ resturant_management/urls.py
@@ -5,8 +5,13 @@ urlpatterns = [
     path("", index),
     # path("home/", home),
     path("index/", index),
-    path("category/", category_list),
-    path("category/<id>", category_detail),
-    path("table/", table_list),
-    path("table/<id>", table_detail),
+    # path("category/", category_list),
+    # path("category/<id>", category_detail),
+    # path("table/", table_list),
+    # path("table/<id>", table_detail),
+    # CLASS BASED
+    # path("category/", CategoryView.as_view()),
+    # path("category/<id>", CategoryView_Detail.as_view()),
+    path("category/", CategoryGeneric.as_view()),
+    path("category/<id>", CategoryGeneric_Detail.as_view()),
 ]
--- resturant_management/views.py
+++ resturant_management/views.py
@@ -7,87 +7,175 @@ from .serializer import *  # importing all serializers(converting queryset to js
 
 # Create your views here.
 
+# Generic API
+from rest_framework.generics import GenericAPIView
 
-@api_view(["GET", "POST"])
-def category_list(request):
-    if request.method == "GET":
-        category = Category.objects.all()
-        serializer = CategorySerializer(category, many=True)
+
+class CategoryGeneric(GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request):  # request handler
+
+        category = self.get_queryset()
+        serializer = self.serializer_class(category, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = CategorySerializer(data=request.data)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message": "Data added", "result": serializer.data})
 
 
-@api_view(["GET", "DELETE", "PUT"])
-def category_detail(request, id):
-    category = Category.objects.get(id=id)
-    if request.method == "GET":
-        serializers = CategorySerializer(category)
+class CategoryGeneric_Detail(GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = "id"
+
+    def get(self, request, id):
+
+        category = self.get_object()
+        serializers = self.serializer_class(category)
         return Response(serializers.data)
 
-    elif request.method == "PUT":
-        serializer = CategorySerializer(category, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
 
-        return Response(
-            {
-                "message": "Data updated successfully",
-                "result": serializer.data,
-            }
-        )
-
-    elif request.method == "DELETE":
-        item = OrderMenu.objects.filter(menu__category=category).count()
-        if item > 0:
-            return Response(
-                {
-                    "message": "Data can not be deleted. Protected Foreign Key in Order Menu"
-                }
-            )
-        category.delete()
-        return Response({"message": "Data has been deleted"})
+# CLASS BASED API
+# from rest_framework.views import APIView
 
 
-@api_view(["GET", "POST"])
-def table_list(request):
-    if request.method == "GET":
-        table = Table.objects.all()
-        serializer = TableSerializer(table, many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = TableSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"message": "Data added", "result": serializer.data})
+# class CategoryView(APIView):
+#     def get(self, request):  # request handler
+
+#         category = Category.objects.all()
+#         serializer = CategorySerializer(category, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+
+#         serializer = CategorySerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"message": "Data added", "result": serializer.data})
 
 
-@api_view(["GET", "DELETE", "PUT"])
-def table_detail(request, id):
-    table = Table.objects.get(id=id)
-    if request.method == "GET":
-        serializers = TableSerializer(table)
-        return Response(serializers.data)
+# class CategoryView_Detail(APIView):
 
-    elif request.method == "PUT":
-        serializer = TableSerializer(table, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+#     def get(self, request, id):
 
-        return Response(
-            {
-                "message": "Data updated successfully",
-                "result": serializer.data,
-            }
-        )
+#         category = Category.objects.get(id=id)
+#         serializers = CategorySerializer(category)
+#         return Response(serializers.data)
 
-    elif request.method == "DELETE":
+#     def put(self, request, id):
 
-        table.delete()
-        return Response({"message": "Data has been deleted"})
+#         category = Category.objects.get(id=id)
+#         serializer = CategorySerializer(category, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response(
+#             {
+#                 "message": "Data updated successfully",
+#                 "result": serializer.data,
+#             }
+#         )
+
+#     def delete(self, request, id):
+
+#         category = Category.objects.get(id=id)
+#         item = OrderMenu.objects.filter(menu__category=category).count()
+#         if item > 0:
+#             return Response(
+#                 {
+#                     "message": "Data can not be deleted. Protected Foreign Key in Order Menu"
+#                 }
+#             )
+#         category.delete()
+#         return Response({"message": "Data has been deleted"})
+
+
+# FUNCTION BASED API
+# @api_view(["GET", "POST"])
+# def category_list(request):
+#     if request.method == "GET":
+#         category = Category.objects.all()
+#         serializer = CategorySerializer(category, many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = CategorySerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"message": "Data added", "result": serializer.data})
+
+
+# @api_view(["GET", "DELETE", "PUT"])
+# def category_detail(request, id):
+#     category = Category.objects.get(id=id)
+#     if request.method == "GET":
+#         serializers = CategorySerializer(category)
+#         return Response(serializers.data)
+
+#     elif request.method == "PUT":
+#         serializer = CategorySerializer(category, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response(
+#             {
+#                 "message": "Data updated successfully",
+#                 "result": serializer.data,
+#             }
+#         )
+
+#     elif request.method == "DELETE":
+#         item = OrderMenu.objects.filter(menu__category=category).count()
+#         if item > 0:
+#             return Response(
+#                 {
+#                     "message": "Data can not be deleted. Protected Foreign Key in Order Menu"
+#                 }
+#             )
+#         category.delete()
+#         return Response({"message": "Data has been deleted"})
+
+
+# @api_view(["GET", "POST"])
+# def table_list(request):
+#     if request.method == "GET":
+#         table = Table.objects.all()
+#         serializer = TableSerializer(table, many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = TableSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"message": "Data added", "result": serializer.data})
+
+
+# @api_view(["GET", "DELETE", "PUT"])
+# def table_detail(request, id):
+#     table = Table.objects.get(id=id)
+#     if request.method == "GET":
+#         serializers = TableSerializer(table)
+#         return Response(serializers.data)
+
+#     elif request.method == "PUT":
+#         serializer = TableSerializer(table, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response(
+#             {
+#                 "message": "Data updated successfully",
+#                 "result": serializer.data,
+#             }
+#         )
+
+#     elif request.method == "DELETE":
+
+#         table.delete()
+#         return Response({"message": "Data has been deleted"})
 
 
 def index(request):
